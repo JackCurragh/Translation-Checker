b614ff3786d9c6a1710d1ba4642edd516ae2db51
Jack Tierney <jack.tierney@student.ucc.ie>
2023-03-16 10:10:05 +0000

    initial push of code

---
+++ Translation-Checker.py
@@ -0,0 +1,219 @@
+"""
+Take a list of genomic ranges and return a score for how much translation support there is for each range.
+
+Input:
+    - A list of genomic ranges (name, chr, start, end)
+    - A Ribo-Seq File (BED or BigWig)
+
+Output:
+    - A list of genomic ranges with a score for how much translation support there is for each range. (chr, start, end, score)
+
+Usage:
+    python Translation-Checker.py -g <genomic ranges file> -r <ribo-seq file> -o <output file> 
+
+Options:
+    -g, --genomic-ranges-file <genomic ranges file>  A list of genomic ranges (chr, start, end)
+    -r, --ribo-seq-file <ribo-seq file>             A Ribo-Seq File (BED or BigWig)
+    -o, --output-file <output file>                 A list of genomic ranges with a score for how much translation support there is for each range. (chr, start, end, score)
+    -h, --help                                      Show this screen.
+"""
+
+import argparse
+import pandas as pd
+import pyBigWig
+
+
+def read_genomic_ranges(genomic_ranges_file: str) -> pd.DataFrame:
+    """
+    Read a list of genomic ranges (name, chr, start, end)
+
+    Parameters
+    ----------
+    genomic_ranges_file : str
+        A list of genomic ranges (name, chr, start, end)
+
+    Returns
+    -------
+    genomic_ranges : pd.DataFrame
+        A dataframe of genomic ranges (name chr, start, end)
+    """
+    genomic_ranges = pd.read_csv(
+        genomic_ranges_file,
+        sep="\t",
+        header=None,
+        names=["name", "chr", "start", "end"],
+    )
+    return genomic_ranges
+
+
+def read_ribo_seq_bed(ribo_seq_file: str) -> pd.DataFrame:
+    """
+    Read a Ribo-Seq File that is in BED format (chr, start, end, score)
+
+    Parameters
+    ----------
+    ribo_seq_file : str
+        A Ribo-Seq File (BED)
+
+    Returns
+    -------
+    ribo_seq : pd.DataFrame
+        A dataframe of ribo-seq data (chr, start, end, score)
+    """
+    try:
+        ribo_seq = pd.read_csv(
+            ribo_seq_file, sep="\t", header=None, names=["chr", "start", "end", "score"]
+        )
+        return ribo_seq
+    except:
+        raise Exception("Ribo-Seq file does not appear to be a BED file")
+
+
+def read_ribo_seq_bigwig(ribo_seq_file: str) -> pyBigWig:
+    """
+    Read a Ribo-Seq File that is in BigWig format
+
+    Parameters
+    ----------
+    ribo_seq_file : str
+        A Ribo-Seq File (BigWig)
+
+    Returns
+    -------
+    ribo_seq : pyBigWig
+        A pyBigWig object of ribo-seq data
+    """
+    bw = pyBigWig.open(ribo_seq_file)
+
+    if bw.isBigWig():
+        return bw
+    else:
+        raise Exception("The file is not a BigWig file")
+
+
+def calculate_translation_support_bed(
+    genomic_ranges: pd.DataFrame, ribo_seq: pd.DataFrame
+) -> pd.DataFrame:
+    """
+    Calculate the translation support for each genomic range
+
+    Parameters
+    ----------
+    genomic_ranges : pd.DataFrame
+        A dataframe of genomic ranges (chr, start, end)
+    ribo_seq : pd.DataFrame
+        A dataframe of ribo-seq data (chr, start, end, score)   
+
+    Returns
+    -------
+    translation_support : pd.DataFrame
+        A dataframe of genomic ranges with a score for how much translation support there is for each range. (chr, start, end, score)
+    """
+    translation_support = pd.DataFrame(columns=["chr", "start", "end", "score"])
+    for index, row in genomic_ranges.iterrows():
+        genomic_range = ribo_seq[
+            (ribo_seq["chr"] == row["chr"])
+            & (ribo_seq["start"] >= row["start"])
+            & (ribo_seq["end"] <= row["end"])
+        ]
+        translation_support = translation_support.append(
+            {
+                "name": row["name"],
+                "chr": row["chr"],
+                "start": row["start"],
+                "end": row["end"],
+                "score": genomic_range["score"].sum(),
+            },
+            ignore_index=True,
+        )
+    return translation_support
+
+
+def calculate_translation_support_bigwig(
+    genome_ranges: pd.DataFrame, bw: pyBigWig
+) -> pd.DataFrame:
+    """
+    Calculate translation support for each genomic range
+
+    Parameters
+    ----------
+    genomic_ranges : pd.DataFrame
+        A dataframe of genomic ranges (chr, start, end)
+    bw : pyBigWig
+        A pyBigWig object of ribo-seq data
+
+    Returns
+    -------
+    translation_support : pd.DataFrame
+        A dataframe of genomic ranges with a score for how much translation support there is for each range. (chr, start, end, score)
+    """
+    translation_support = pd.DataFrame(columns=["chr", "start", "end", "score"])
+    for index, row in genome_ranges.iterrows():
+        genomic_range = bw.stats(row["chr"], row["start"], row["end"], type="sum")
+        translation_support = translation_support.append(
+            {
+                "chr": row["chr"],
+                "start": row["start"],
+                "end": row["end"],
+                "score": genomic_range[0],
+            },
+            ignore_index=True,
+        )
+    return translation_support
+
+
+def write_output(translation_support: pd.DataFrame, output_file: str):
+    """
+    Write output to specified filepath
+    """
+    translation_support.to_csv(output_file, sep="\t", header=None, index=False)
+
+
+def main(args: argparse.Namespace):
+    """
+    Run translation checker 
+    """
+    genomic_ranges = read_genomic_ranges(args.genomic_ranges_file)
+
+    file_is_bw = pyBigWig.open(args.ribo_seq_file).isBigWig()
+
+    if file_is_bw:
+        ribo_seq = read_ribo_seq_bigwig(args.ribo_seq_file)
+        translation_support = calculate_translation_support_bigwig(
+            genomic_ranges, ribo_seq
+        )
+    else:
+        ribo_seq = read_ribo_seq_bed(args.ribo_seq_file)
+        translation_support = calculate_translation_support_bed(
+            genomic_ranges, ribo_seq
+        )
+    write_output(translation_support, args.output_file)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="Take a list of genomic ranges and return a score for how much translation support there is for each range."
+    )
+    parser.add_argument(
+        "-g",
+        "--genomic-ranges-file",
+        type=str,
+        required=True,
+        help="A list of genomic ranges (chr, start, end)",
+    )
+    parser.add_argument(
+        "-r",
+        "--ribo-seq-file",
+        type=str,
+        required=True,
+        help="A Ribo-Seq File (BED or BigWig)",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-file",
+        type=str,
+        required=True,
+        help="A list of genomic ranges with a score for how much translation support there is for each range. (chr, start, end, score)",
+    )
+    args = parser.parse_args()
+    main(args)
---
+++ scripts/UCSC_BED_to_ranges_csv.py
@@ -0,0 +1,85 @@
+'''
+Script to convert a bed file outputted from GWIPS-viz or UCSC genome browser to a csv file of genomic ranges
+
+Usage:
+    python3 bed_to_ranges_csv.py -i <bed file> -o <output file>
+
+Options:
+    -i  --bed-file <bed file>           A bed file
+    -o, --output-file <output file>      A bed file
+    -h, --help                           Show this screen.
+
+'''
+
+import argparse
+import pandas as pd
+
+def read_bed(bed: str) -> pd.DataFrame:
+    '''
+    Read a bed file
+
+    Parameters
+    ----------
+    bed : str
+        A bed file
+
+    Returns
+    -------
+    bed : pd.DataFrame
+        A dataframe of bed data
+    '''
+    column_names = ['chr', 'start', 'end', 'name', 'score', 'strand', 'thickStart', 'thickEnd', 'itemRgb', 'blockCount', 'blockSizes', 'blockStarts']
+    bed = pd.read_csv(bed, sep='\t', header=None, names=column_names)
+    return bed
+
+def bed_to_csv(bed: pd.DataFrame) -> pd.DataFrame:
+    '''
+    Convert a bed dataframe to a csv dataframe of genomic ranges
+
+    Parameters
+    ----------
+    bed : pd.DataFrame
+        A dataframe of bed data
+
+    Returns
+    -------
+    bed : pd.DataFrame
+        A dataframe of bed data for genomic coding regions
+    '''
+    genomic_ranges = pd.DataFrame(columns=['name', 'chr', 'start', 'end'])
+
+    for index, row in bed.iterrows():
+        for i in range(row['blockCount']):
+            start = row['start'] + row['blockStarts'].split(',')[i]
+            end = start + row['blockSizes'].split(',')[i]
+            print({'name': row['name'], 'chr': row['chr'], 'start': start, 'end': end})
+            genomic_ranges = genomic_ranges.append({'name': row['name'], 'chr': row['chr'], 'start': start, 'end': end}, ignore_index=True)
+
+    return bed
+
+def write_bed(bed: pd.DataFrame, output_file: str):
+    '''
+    Write a bed dataframe to a file
+
+    Parameters
+    ----------
+    bed : pd.DataFrame
+        A dataframe of bed data
+    output_file : str
+        A bed file
+    '''
+    bed.to_csv(output_file, sep='\t', header=False, index=False)
+
+def main():
+    bed = read_bed(args.input)
+    bed = bed_to_csv(bed)
+    write_bed(bed, args.output)
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Script to convert a genePred file to a bed file')
+    parser.add_argument('-i', '--input', help='A genePred file', required=True)
+    parser.add_argument('-o', '--output', help='A bed file', required=True)
+    args = parser.parse_args()
+
